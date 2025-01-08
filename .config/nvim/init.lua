320e87ea74c5a8dd2a95251cb28c62ab03b71be7
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2025-01-08 11:17:57 +0700

    nvim: auto session

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -975,6 +975,7 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   require 'custom.plugins.vim-tmux-navigator',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.auto-session',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
