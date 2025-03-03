4fd0fc8b3935d94f3bef3ab91a4c0954e4bdd522
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2025-03-03 09:42:44 +0700

    nvim: session search keybind

--- .config/nvim/lua/kickstart/plugins/auto-session.lua
+++ .config/nvim/lua/kickstart/plugins/auto-session.lua
@@ -2,6 +2,12 @@ return {
   'rmagatti/auto-session',
   lazy = false,
 
+  keys = {
+    { '<leader>ss', '<cmd>SessionSearch<CR>', desc = '[S]ession [S]earch' },
+  },
+
+  ---@module "auto-session"
+  ---@type AutoSession.Config
   opts = {
     suppressed_dirs = { '~/', '~/Downloads', '/' },
   },
