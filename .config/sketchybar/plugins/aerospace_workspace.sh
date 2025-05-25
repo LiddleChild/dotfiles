405c10c0b5eb171fc583a43e30ace1fc4f525e0c
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2025-05-25 10:45:19 +0700

    sketchybar: speed up workspace sync

--- .config/sketchybar/plugins/aerospace_workspace.sh
+++ .config/sketchybar/plugins/aerospace_workspace.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-if [ "$1" = "$(aerospace list-workspaces --focused)" ]; then
+if [ -z $FOCUSED_WORKSPACE ]; then
+    FOCUSED_WORKSPACE=$(aerospace list-workspaces --focused)
+fi
+
+if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
     sketchybar --set $NAME background.color=$3 label.color=$2 background.drawing=true
 else
     sketchybar --set $NAME background.color=$2 label.color=$3 background.drawing=false
