fab552cc370836362a638c61eb83e762882177ab
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2025-05-25 15:19:25 +0700

    sketchybar: app icon

--- .config/sketchybar/plugins/front_app.sh
+++ .config/sketchybar/plugins/front_app.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if [ "$SENDER" = "front_app_switched" ]; then
-  sketchybar --set "$NAME" label="$INFO"
+  sketchybar --set "$NAME" label="$INFO" background.image="app.$INFO"
 fi
